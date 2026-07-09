72eb11487ea33e2c6cf888d928f8f3f9f8923f1e
Aishik ghosh <aishikghosh14@gmail.com>
2026-07-09 16:52:11 +0530

    feat(sql): add startup database creation script

---
+++ sql/database_startup.sql
@@ -0,0 +1,14 @@
+-- To check whether the database exists in the user's server and return it
+SHOW DATABASES 
+LIKE 'inventory_management'
+
+--To create the database
+CREATE DATABASE 
+IF NOT EXISTS 'inventory_management'
+
+--To use the database
+USE DATABASE
+
+--To view the tables present in the database
+SHOW TABLES
+
